90eea740e20134f0c7c333b71e790dea7a8d9746
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-06 15:20:38 +1000

    Forms and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,38 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+
+
 socket.on('sameName', (messageFromServer) => {
     console.log(messageFromServer)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    //disable
+    $messageFormButton.setAttribute('disabled','disabled')
+
     const messageToServer = e.target.elements.message.value
     socket.emit('commonName', messageToServer, (callbackMessageFromServer) => {
+        
+        //enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if(callbackMessageFromServer){
             return console.log(callbackMessageFromServer)
         }
         console.log('Message delivered!')
     })
 })
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
+    $locationButton.setAttribute('disabled','disabled')
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browner')
     }
@@ -22,6 +40,9 @@ document.querySelector('#send-location').addEventListener('click', () => {
         socket.emit('sendLocation',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        }, (callbackMessageFromServer) => console.log(callbackMessageFromServer))
+        }, (callbackMessageFromServer) => {
+            $locationButton.removeAttribute('disabled')    
+            console.log(callbackMessageFromServer)
+        })
     })
 }) 
